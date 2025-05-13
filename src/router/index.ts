9f2b2eaee10b950a3a3f93344f3d3d8d0b378d43
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2025-05-13 16:32:03 +0700

    initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import SignupView from '@/views/SignupView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+  ],
+})
+
+export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>This is a Log In page.</h1>
+</template>
