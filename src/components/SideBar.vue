611c727a412fb98cdd6b4c0565a3b2a2a2b32c86
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2025-08-07 04:22:02 +0700

    add todo page to side bar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -120,6 +120,23 @@
             <span class="hidden lg:inline">Plan</span>
           </a>
       </router-link>
+
+      <router-link to="/todo" custom v-slot="{ href, navigate, isActive }">
+        <a
+            :href="href"
+            @click="navigate"
+            :class="[
+              'flex items-center gap-4 p-3 rounded-lg',
+              isActive
+                ? 'bg-[#4454C0] font-semibold'
+                : 'hover:bg-white/10',
+            ]"
+            title="Todo"
+          >
+            <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" /></svg>
+            <span class="hidden lg:inline">To-do List</span>
+          </a>
+      </router-link>
     </nav>
 
     <div class="mt-auto shrink-0">
